306157890abbf705837526f886bfebda0f7a3bd5
Kenneth Kimani <kuriakimani@yahoo.com>
2016-11-03 22:43:59 +0300

    A real world problem modelled using OOP
    Github repo containing a real world problem modelled using OOP while taking advantage of inheriatnce, encapsulation, polymorphism and the other OOP concepts.

--- Self-Learning_Home-Day-One/real-world-problem_OOP.py
+++ Self-Learning_Home-Day-One/real-world-problem_OOP.py
@@ -1,17 +1,44 @@
+import datetime
 
 class BlindDate(object):
     """
    Evaluation code for applicants to join a dating site.
     """
 
-    def __init__(self, name, gender, age, marital_status, age_find, application_complete):
+    TITLES = ('Dr', 'Mr', 'Mrs', 'Ms')
+
+    def __init__(self, name, surname, gender, age, birthdate, marital_status, age_find, email, application_complete):
         self.name = name
+        self.surname = surname
         self.gender = gender
         self.age = age
+        self.birthdate = birthdate
         self.age_find = age_find
+        self.email = email
         self.marital_status = marital_status
         self.application_complete = application_complete
 
+    def fullname(self, title):
+        if title not in self.TITLES:
+            raise ValueError("Unrecognised title: '%s'" % title)
+
+        return "%s %s %s" % (title, self.name, self.surname)
+
+    def years_old(self):
+        """This method calculates the person's age from the birthdate and the current date.
+
+        :returns: int -- the person's age in years
+        """
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+
+        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+            age -= 1
+
+        return age
+
+
+    # check if application complete
     def join_application(self, age, application_complete):
         if application_complete == "Yes" and age >= 18:
             return "Application accepted! Welcome to Dating Site. You are Eligible for a blind date"
@@ -20,6 +47,7 @@ class BlindDate(object):
         else:
             return "Application for a blind date not Successful"
 
+    # check the marital status of potential members and advice accordingly
     def match_maker(self, marital_status):
         if marital_status == 'Single':
             return "A potential soul mate is waiting to go on a date with you"
@@ -30,6 +58,7 @@ class BlindDate(object):
         else:
             return "Yea, yea, we understand. Its complicated."
 
+    # match user and partner depending on age preference
     def matchup(self, age, age_find):
         if age > 0 and age < 18:
             return "Sorry no minors on this site. You belong in jail Paedophile. "
@@ -42,6 +71,7 @@ class BlindDate(object):
         else:
             return "Sorry but you entered an incorrect value for potential date partner"
 
+    # confirm man settling bill
     def bill_payment(self, gender):
         if gender == 'Male':
             return "Please remember you will be footing the bill for the date"
@@ -72,8 +102,15 @@ class Marital_Status(BlindDate):
         return self.match_maker
 
 
-user = BlindDate("Mike", "Male", 25, "Single", 23, "Yes")
+user = BlindDate("Mike", "Makonde", "Male", 25, datetime.date(1990, 11, 22), "Single", 23, "mikem@gmail.com", "Yes")
+
+if user.years_old() != user.age:
+    raise RuntimeError('You lied about your age!!.')
+else:
+    print "Age Confirmed!!"
+
 print user.application_complete
 print user.age
 print Login.years
+
 
